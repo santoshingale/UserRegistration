b6814fa8fc17a5d346e8f4584ab2be7114cd8213
santoshingale <santoshingale876@gmail.com>
2019-12-24 12:28:16 +0530

    Modified : Test cases for checkFirstName function

--- src/main/java/com/userregistration/UserRegistration.java
+++ src/main/java/com/userregistration/UserRegistration.java
@@ -4,6 +4,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
+
     public static boolean checkFirstName(String firstName) {
         Pattern firstNamePattern = Pattern.compile("[A-Z]{1}[a-z]{2,}");
         Matcher m = firstNamePattern.matcher(firstName);
--- src/test/java/UserRegistrationTest.java
+++ src/test/java/UserRegistrationTest.java
@@ -4,17 +4,18 @@ import org.junit.Test;
 
 public class UserRegistrationTest {
     @Test
-    public void whenGivenFistName_withThreeCharacter_withFirstCapitalLetter_shouldReturnValid() {
+    public void whenGivenFistName_withThreeCharacter_withFirstCapitalLetter_shouldReturnTrue() {
         Assert.assertTrue(UserRegistration.checkFirstName("Sam"));
     }
 
     @Test
-    public void whenGivenFistName_withoutThreeCharacter_withFirstCapitalLetter_shouldReturnInvalid() {
-        Assert.assertTrue(UserRegistration.checkFirstName("Sa"));
+    public void whenGivenFistName_withoutThreeCharacter_withFirstCapitalLetter_shouldReturnFalse() {
+        Assert.assertFalse(UserRegistration.checkFirstName("Sa"));
     }
 
     @Test
-    public void whenGivenFistName_withThreeCharacter_withoutFirstCapitalLetter_shouldReturnInvalid() {
-        Assert.assertTrue(UserRegistration.checkFirstName("san"));
+    public void whenGivenFistName_withThreeCharacter_withoutFirstCapitalLetter_shouldReturnFalse() {
+        Assert.assertFalse(UserRegistration.checkFirstName("san"));
     }
+
 }
